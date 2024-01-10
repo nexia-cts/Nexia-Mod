2461ee992992a4c8f3e1a98ca2c8b94b1bebe564
surgeflame <76570284+surgeflame@users.noreply.github.com>
2024-01-10 10:54:12 -0500

    Update OitcMap.java

--- src/main/java/com/nexia/minigames/games/oitc/OitcMap.java
+++ src/main/java/com/nexia/minigames/games/oitc/OitcMap.java
@@ -1,39 +1,17 @@
 package com.nexia.minigames.games.oitc;
 
-import com.nexia.core.utilities.pos.EntityPos;
-import net.minecraft.core.BlockPos;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class OitcMap {
 
     public static List<OitcMap> oitcMaps = new ArrayList<>();
 
+    public static List<String> stringOitcMaps = new ArrayList<>();
+
     public String id;
 
-    public String name;
-
-    public BlockPos corner1;
-
-    public BlockPos corner2;
-
-    public int maxPlayers;
-    public ArrayList<EntityPos> spawnPositions;
-
-    public static OitcMap CITY = new OitcMap("city", "City", 8, new BlockPos(0, 0, 0), new BlockPos(0, 0, 0),
-            new ArrayList<>(Arrays.asList(
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0),
-                    new EntityPos(0, 80, 0,0,0)
-            ))
-    );
+    public static OitcMap CITY = new OitcMap("city");
 
 
     public static OitcMap identifyMap(String name) {
@@ -43,15 +21,10 @@ public class OitcMap {
         return null;
     }
 
-    public OitcMap(String id, String name, int maxPlayers, BlockPos corner1, BlockPos corner2, ArrayList<EntityPos> spawnPositions) {
+    public OitcMap(String id) {
         this.id = id;
-        this.name = name;
-        this.maxPlayers = maxPlayers;
-        this.spawnPositions = spawnPositions;
 
-        this.corner1 = corner1;
-        this.corner2 = corner2;
-
+        OitcMap.stringOitcMaps.add(id);
         OitcMap.oitcMaps.add(this);
     }
 }
