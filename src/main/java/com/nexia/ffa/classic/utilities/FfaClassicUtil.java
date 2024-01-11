02f5a75578c53b5422f5b3714f9b6a6820d0dd69
surgeflame <flameninjasand@gmail.com>
2024-01-11 12:21:58 -0500

    Γ

--- src/main/java/com/nexia/ffa/classic/utilities/FfaClassicUtil.java
+++ src/main/java/com/nexia/ffa/classic/utilities/FfaClassicUtil.java
@@ -210,7 +210,7 @@ public class FfaClassicUtil {
             } else if (handItem == Items.DIAMOND_AXE) {
                 symbol = "\uD83E\uDE93";
             } else if (handItem == Items.DIAMOND_HOE) {
-                symbol = "∫";
+                symbol = "Γ";
             }
 
             msg = Component.text("☠ " + minecraftPlayer.getScoreboardName()).color(ChatFormat.failColor)
