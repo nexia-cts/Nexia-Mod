a66f1c49487bc72a9ca7a0450a8c532a03a8c9fa
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-03-01 18:05:15 +0100

    alright fuck that

--- src/main/java/com/nexia/core/utilities/time/ServerType.java
+++ src/main/java/com/nexia/core/utilities/time/ServerType.java
@@ -1,35 +1,22 @@
 package com.nexia.core.utilities.time;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import com.nexia.core.Main;
 
 public class ServerType {
 
     public String type;
-    public String domain;
-    public static ServerType EU = new ServerType("eu", "eu.nexia.dev");
+    public static ServerType EU = new ServerType("eu");
 
-    public static ServerType NA = new ServerType("na", "example.com"); // na.nexia.dev
+    public static ServerType NA = new ServerType("na");
 
-    public static ServerType DEV = new ServerType("dev", "dev.nexia.dev");
+    public static ServerType DEV = new ServerType("dev");
 
-    public ServerType(String type, String domain) {
+    public ServerType(String type) {
         this.type = type;
-        this.domain = domain;
-    }
-
-    public String getIP() throws UnknownHostException {
-        return InetAddress.getByName(this.domain).getHostAddress();
     }
 
     public static ServerType returnServer() {
-        String ip = ServerTime.minecraftServer.getLocalIp();
-        try {
-            if(ip.equalsIgnoreCase(ServerType.EU.getIP())) return ServerType.EU;
-            else if(ip.equalsIgnoreCase(ServerType.NA.getIP())) return ServerType.NA;
-            else if(ip.equalsIgnoreCase(ServerType.DEV.getIP())) return ServerType.DEV;
-        } catch (UnknownHostException ignored) { }
-        return null;
+        return getServerType(Main.config.serverType);
     }
 
     public static ServerType getServerType(String region) {
