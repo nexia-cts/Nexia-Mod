b18dc2a33dbc5a971c73929a3f9d3c4fb5216b3e
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-03-01 17:14:08 +0100

    Squashed commit of the following:
    commit daf1c5a56999f3edf7c08a6dda3f59cb86ab90c7
    Author: NotCoded <66999075+not-coded@users.noreply.github.com>
    Date:   Fri Mar 1 17:13:46 2024 +0100
    
        fix

--- src/main/java/com/nexia/core/mixin/player/ServerLoginPacketListenerMixin.java
+++ src/main/java/com/nexia/core/mixin/player/ServerLoginPacketListenerMixin.java
@@ -21,7 +21,7 @@ public class ServerLoginPacketListenerMixin {
 
     @ModifyArg(method = "handleAcceptedLogin", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;disconnect(Lnet/minecraft/network/chat/Component;)V"))
     private Component handleAcceptedLogin(Component original) {
-        if ((!(original instanceof TranslatableComponent component)) || ServerType.returnServer() != ServerType.DEV) return original;
+        if ((!(original instanceof TranslatableComponent component))) return original;
 
         if (component.getKey().contains("banned")) {
             component = new TranslatableComponent("§c§lYou have been banned.\n§7Reason: §d" + component.getString().split("Reason: ")[1] + "\n§7You can appeal your ban at §d" + Main.config.discordLink);
