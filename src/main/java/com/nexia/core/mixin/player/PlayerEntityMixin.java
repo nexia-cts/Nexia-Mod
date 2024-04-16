4b5c0ddb4f846d75d57a03b9361414829a72b94d
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-04-16 23:36:27 +0200

    adding sprint hit fix

--- src/main/java/com/nexia/core/mixin/player/PlayerEntityMixin.java
+++ src/main/java/com/nexia/core/mixin/player/PlayerEntityMixin.java
@@ -1,22 +1,21 @@
 package com.nexia.core.mixin.player;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Mixin(Player.class)
 public class PlayerEntityMixin {
-    public final PlayerEntity player = PlayerEntity.class.cast(this);
+    public final Player player = Player.class.cast(this);
     public PlayerEntityMixin() {
     }
 
-    @Inject(
+    /*@Inject(
             method = "attack",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V", shift = At.Shift.AFTER)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setVelocity(Lnet/minecraft/util/math/Vec3d;)V", shift = At.Shift.AFTER)
     )
     public void attack2(Entity target, CallbackInfo ci) {
         if (isPlayerSprintFixed.get(player.getUuid())) {
@@ -24,14 +23,18 @@ public class PlayerEntityMixin {
             double z = player.getVelocity().getZ() / 0.6;
             player.setVelocity(x, player.getVelocity().getY(), z);
         }
-    }
+    }*/
 
     @Inject(
             method = "attack",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V", shift = At.Shift.AFTER)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setSprinting(Z)V", shift = At.Shift.AFTER)
     )
     public void attack3(Entity target, CallbackInfo ci) {
-        if (isPlayerSprintFixed.get(player.getUuid()))
-            player.setSprinting(true);
+        for (String tag : player.getTags()) {
+            if (tag == "sprintfixdisable") {
+                return;
+            }
+        }
+        player.setSprinting(true);
     }
 }
