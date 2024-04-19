01f4589e19a26fa330a802d6aa562c7a9b58bf73
surgeflame <flameninjasand@gmail.com>
2024-04-19 12:04:48 -0400

    Reaper kit bs

--- src/main/java/com/nexia/ffa/kits/FfaKit.java
+++ src/main/java/com/nexia/ffa/kits/FfaKit.java
@@ -20,9 +20,10 @@ public class FfaKit {
 
     public static final FfaKit KNIGHT = new FfaKit("knight", new ItemStack(Items.DIAMOND_SWORD));
     public static final FfaKit POSEIDON = new FfaKit("poseidon", new ItemStack(Items.TRIDENT));
-    public static final FfaKit BRUTE = new FfaKit("brute", new ItemStack(Items.NETHERITE_CHESTPLATE));
+    public static final FfaKit BRUTE = new FfaKit("brute", new ItemStack(Items.NETHERITE_AXE));
     public static final FfaKit HUNTER = new FfaKit("hunter", new ItemStack(Items.CROSSBOW));
     public static final FfaKit NINJA = new FfaKit("ninja", new ItemStack(Items.SUGAR));
+    public static final FfaKit REAPER = new FfaKit("reaper", new ItemStack(Items.SUGAR));
 
     public FfaKit(String id, ItemStack item) {
         this.id = id;
@@ -40,6 +41,7 @@ public class FfaKit {
             case "brute" -> FfaKit.BRUTE;
             case "hunter" -> FfaKit.HUNTER;
             case "ninja" -> FfaKit.NINJA;
+            case "reaper" -> FfaKit.REAPER;
             default -> null;
         };
          */
