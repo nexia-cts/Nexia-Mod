646129e775f386af4cf3fac9d1ce2d0567b1cabb
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-04-21 23:04:25 +0200

    please work

---
+++ src/main/java/com/nexia/core/mixin/block/ChestBlockMixin.java
@@ -0,0 +1,34 @@
+package com.nexia.core.mixin.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ChestBlock.class)
+public class ChestBlockMixin {
+    @Unique
+    CompoundTag compoundTag = new CompoundTag();
+
+    @Inject(method = "use", at = @At("HEAD"))
+    public void use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        // TODO add a check if the player is in the actual kit room
+        level.getBlockEntity(blockPos).save(compoundTag);
+    }
+
+    @Inject(method = "use", at = @At("TAIL"))
+    public void use2(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        // TODO add a check if the player is in the actual kit room
+        level.getBlockEntity(blockPos).load(blockState, compoundTag);
+    }
+}
