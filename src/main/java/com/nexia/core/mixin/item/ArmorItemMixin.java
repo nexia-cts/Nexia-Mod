cddbc7e50686f7ca883c6638060df078c8c1aa89
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-04-22 11:46:40 +0200

    armor swapping

---
+++ src/main/java/com/nexia/core/mixin/item/ArmorItemMixin.java
@@ -0,0 +1,34 @@
+package com.nexia.core.mixin.item;
+
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ArmorItem.class)
+public class ArmorItemMixin {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    public void use(Level level, Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        EquipmentSlot equipmentSlot = Mob.getEquipmentSlotForItem(itemStack);
+        ItemStack itemStack2 = player.getItemBySlot(equipmentSlot);
+        if (EnchantmentHelper.hasBindingCurse(itemStack2) && !player.isCreative() || ItemStack.isSame(itemStack, itemStack2)) {
+            cir.setReturnValue(InteractionResultHolder.fail(itemStack));
+        }
+
+        ItemStack itemStack3 = itemStack2.isEmpty() ? itemStack : itemStack2.copy();
+        ItemStack itemStack4 = itemStack.copy();
+        player.setItemSlot(equipmentSlot, itemStack4);
+        cir.setReturnValue(InteractionResultHolder.success(itemStack3));
+    }
+}
