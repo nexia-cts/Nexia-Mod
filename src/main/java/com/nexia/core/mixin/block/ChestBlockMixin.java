0b17da68838530c06fd5e3d367132ee6ee20986d
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-04-22 11:09:23 +0200

    this is horrible lol

--- src/main/java/com/nexia/core/mixin/block/ChestBlockMixin.java
+++ src/main/java/com/nexia/core/mixin/block/ChestBlockMixin.java
@@ -2,33 +2,50 @@ package com.nexia.core.mixin.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stat;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ChestBlock.class)
-public class ChestBlockMixin {
-    @Unique
-    CompoundTag compoundTag = new CompoundTag();
+public abstract class ChestBlockMixin {
+    @Shadow @Nullable public abstract MenuProvider getMenuProvider(BlockState blockState, Level level, BlockPos blockPos);
 
-    @Inject(method = "use", at = @At("HEAD"))
-    public void use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        // TODO add a check if the player is in the actual kit room
+    @Shadow protected abstract Stat<ResourceLocation> getOpenChestStat();
+
+    /**
+     * @author infinityy
+     * @reason figure out a better way to do this lol
+     */
+    @Overwrite()
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        CompoundTag compoundTag = new CompoundTag();
+
         level.getBlockEntity(blockPos).save(compoundTag);
-    }
 
-    @Inject(method = "use", at = @At("TAIL"))
-    public void use2(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        // TODO add a check if the player is in the actual kit room
-        level.getBlockEntity(blockPos).load(blockState, compoundTag);
+        if (level.isClientSide) {
+            return InteractionResult.SUCCESS;
+        } else {
+            MenuProvider menuProvider = this.getMenuProvider(blockState, level, blockPos);
+            if (menuProvider != null) {
+                player.openMenu(menuProvider);
+                player.awardStat(this.getOpenChestStat());
+                PiglinAi.angerNearbyPiglins(player, true);
+            }
+
+            level.getBlockEntity(blockPos).load(blockState, compoundTag);
+            return InteractionResult.CONSUME;
+        }
     }
 }
