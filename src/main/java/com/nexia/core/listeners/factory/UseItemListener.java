08a028941f043b76c712e70efebe7bba8473352a
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-04-27 20:58:32 +0200

    fix custom duel sword being used for queue

--- src/main/java/com/nexia/core/listeners/factory/UseItemListener.java
+++ src/main/java/com/nexia/core/listeners/factory/UseItemListener.java
@@ -37,7 +37,7 @@ public class UseItemListener {
                     return;
                 }
 
-                if(sName.contains("duel sword")) {
+                if(sName.contains("duel sword") && !sName.contains("custom duel sword")) {
                     //QueueGUI.openQueueGUI(minecraftPlayer);
                     player.runCommand("/queue", 0, false);
                     return;
