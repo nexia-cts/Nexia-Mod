38971f7b3b373ac1ebb62828f42d58e9c8f1dd9a
surgeflame <flameninjasand@gmail.com>
2024-05-01 15:00:08 -0400

    Update DuelsMap.java

--- src/main/java/com/nexia/minigames/games/duels/map/DuelsMap.java
+++ src/main/java/com/nexia/minigames/games/duels/map/DuelsMap.java
@@ -33,7 +33,7 @@ public class DuelsMap {
     public static final DuelsMap NETHFLAT = new DuelsMap("nethflat", true, new ItemStack(Items.NETHERITE_BLOCK), new EntityPos(0, 80, -41, 0, 0), new EntityPos(0, 80, 41 ,180, 0), new StructureMap(new Identifier("duels", "nethflat"), Rotation.NO_ROTATION, true, new BlockPos(0, 80, 0), new BlockPos(-36, -3, -51), true));
     public static final DuelsMap PLAINS = new DuelsMap("plains", true, new ItemStack(Items.GRASS_BLOCK), new EntityPos(-71, 80, -16, 0, 0), new EntityPos(-71, 80, 34, 180, 0), new StructureMap(new Identifier("duels", "plains"), Rotation.CLOCKWISE_90, true, new BlockPos(0, 80, 0), new BlockPos(-40, -20, -31), true));
     public static final DuelsMap EDEN = new DuelsMap("eden", false, new ItemStack(Items.ALLIUM), new EntityPos(55, 80, 0, 90, 0), new EntityPos(-55, 80, 0, -90, 0), new StructureMap(new Identifier("duels", "eden"), Rotation.NO_ROTATION, true, new BlockPos(0, 80, 0), new BlockPos(-62, -7, -23), true));
-    public static final DuelsMap CASTLE = new DuelsMap("castle", false, new ItemStack(Items.CRACKED_STONE_BRICKS), new EntityPos(0, 240, 25, -180, 0), new EntityPos(0, 240, -25, 0, 0), new StructureMap(new Identifier("duels", "castle"), Rotation.NO_ROTATION, true, new BlockPos(0, 216, 0), new BlockPos(-11, -4, -30), true));
+    public static final DuelsMap CASTLE = new DuelsMap("castle", false, new ItemStack(Items.CRACKED_STONE_BRICKS), new EntityPos(0, 220, 25, -180, 0), new EntityPos(0, 220, -25, 0, 0), new StructureMap(new Identifier("duels", "castle"), Rotation.NO_ROTATION, true, new BlockPos(0, 216, 0), new BlockPos(-11, -4, -30), true));
 
     public static DuelsMap identifyMap(String name) {
         for(DuelsMap map : DuelsMap.duelsMaps) {
