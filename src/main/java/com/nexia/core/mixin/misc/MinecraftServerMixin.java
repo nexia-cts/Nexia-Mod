771b79e5584285df0c5a32691df923c3a44cbe3e
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-05-07 18:28:10 +0200

    fix that

--- src/main/java/com/nexia/core/mixin/misc/MinecraftServerMixin.java
+++ src/main/java/com/nexia/core/mixin/misc/MinecraftServerMixin.java
@@ -1,16 +1,19 @@
 package com.nexia.core.mixin.misc;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.authlib.GameProfile;
 import com.nexia.core.Main;
 import com.nexia.core.utilities.time.ServerTime;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
@@ -36,9 +39,10 @@ public class MinecraftServerMixin {
         ServerTime.stopServer();
     }
 
-    @ModifyReturnValue(method = "isUnderSpawnProtection", at = @At("HEAD"))
-    private boolean noSpawnProtection() {
-        return false;
+    @Inject(method = "isUnderSpawnProtection", at = @At("HEAD"), cancellable = true)
+    private void noSpawnProtection(ServerLevel serverLevel, BlockPos blockPos, Player player, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+        return;
     }
 
     @ModifyArg(method = "tickServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/status/ServerStatus$Players;setSample([Lcom/mojang/authlib/GameProfile;)V"))
