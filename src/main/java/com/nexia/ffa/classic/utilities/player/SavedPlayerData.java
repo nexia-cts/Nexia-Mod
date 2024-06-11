9bd33003a2d0fb5e7454bd123753450975a755d6
surgeflame <flameninjasand@gmail.com>
2024-06-10 21:42:50 -0400

    fine we just using fucking elo ig

--- src/main/java/com/nexia/ffa/classic/utilities/player/SavedPlayerData.java
+++ src/main/java/com/nexia/ffa/classic/utilities/player/SavedPlayerData.java
@@ -6,18 +6,13 @@ public class SavedPlayerData {
     public int bestKillstreak;
     public int deaths;
     public double rating;
-    public double relative_increase;
-    public double relative_decrease;
     public SavedPlayerData() {
         this.kills = 0;
         this.killstreak = 0;
         this.bestKillstreak = 0;
-
         this.deaths = 0;
 
-        this.rating = 1;
-        this.relative_increase = 0;
-        this.relative_decrease = 0;
+        this.rating = 100;
     }
 }
 
