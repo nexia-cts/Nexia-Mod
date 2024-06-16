e145120da6a921f0181a8ec168665840a4806a70
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-06-16 19:32:01 +0200

    surge wtf is this

--- src/main/java/com/nexia/ffa/kits/FfaKit.java
+++ src/main/java/com/nexia/ffa/kits/FfaKit.java
@@ -5,7 +5,6 @@ import com.nexia.core.utilities.item.InventoryUtil;
 import com.nexia.core.utilities.misc.RandomUtil;
 import com.nexia.core.utilities.player.PlayerUtil;
 import com.nexia.ffa.kits.utilities.player.PlayerDataManager;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -23,18 +22,18 @@ public class FfaKit {
 
     public ItemStack item;
 
-    public static final FfaKit KNIGHT = new FfaKit("Knight", new ItemStack(Items.DIAMOND_SWORD));
-    public static final FfaKit POSEIDON = new FfaKit("Poseidon", new ItemStack(Items.TRIDENT));
-    public static final FfaKit BRUTE = new FfaKit("Brute", new ItemStack(Items.DIAMOND_AXE));
-    public static final FfaKit NINJA = new FfaKit("Ninja", new ItemStack(Items.SUGAR));
-    public static final FfaKit VIKING = new FfaKit("Viking", new ItemStack(Items.SHIELD));
-    public static final FfaKit ARCHER = new FfaKit("Archer", new ItemStack(Items.BOW));
-    public static final FfaKit HUNTER = new FfaKit("Hunter", new ItemStack(Items.CROSSBOW));
-    public static final FfaKit FARMER = new FfaKit("Farmer", new ItemStack(Items.DIAMOND_HOE));
-    public static final FfaKit RANDOM = new FfaKit("Random", new ItemStack(Items.REDSTONE));
+    public static final FfaKit KNIGHT = new FfaKit("knight", new ItemStack(Items.DIAMOND_SWORD));
+    public static final FfaKit POSEIDON = new FfaKit("poseidon", new ItemStack(Items.TRIDENT));
+    public static final FfaKit BRUTE = new FfaKit("nrute", new ItemStack(Items.DIAMOND_AXE));
+    public static final FfaKit NINJA = new FfaKit("ninja", new ItemStack(Items.SUGAR));
+    public static final FfaKit VIKING = new FfaKit("viking", new ItemStack(Items.SHIELD));
+    public static final FfaKit ARCHER = new FfaKit("archer", new ItemStack(Items.BOW));
+    public static final FfaKit HUNTER = new FfaKit("hunter", new ItemStack(Items.CROSSBOW));
+    public static final FfaKit FARMER = new FfaKit("farmer", new ItemStack(Items.DIAMOND_HOE));
+    public static final FfaKit RANDOM = new FfaKit("random", new ItemStack(Items.REDSTONE));
 
     // Keep track of previously selected kits for each player
-    private static Map<ServerPlayer, String> previousKits = new HashMap<>();
+    private static final Map<ServerPlayer, String> previousKits = new HashMap<>();
 
     public FfaKit(String id, ItemStack item) {
         this.id = id;
@@ -59,34 +58,23 @@ public class FfaKit {
 
         if (this.equals(FfaKit.RANDOM)) {
             ArrayList<String> availableKits = new ArrayList<>(stringFfaKits);
-            availableKits.remove(RANDOM.id); // Remove "RANDOM" from the list of available kits
+            availableKits.remove(RANDOM.id);
 
-            // Get the previously selected kit for this player
             String previousKit = previousKits.getOrDefault(player, "");
-
-            // Remove the previously selected kit from available kits
             availableKits.remove(previousKit);
 
             String selectedKit;
             if (!availableKits.isEmpty()) {
                 selectedKit = availableKits.get(RandomUtil.randomInt(availableKits.size()));
             } else {
-                // If no available kits (all kits were chosen before), select a random one
                 selectedKit = stringFfaKits.get(RandomUtil.randomInt(stringFfaKits.size()));
             }
 
             // Store the selected kit as the previous kit for this player
             previousKits.put(player, selectedKit);
-
             InventoryUtil.loadInventory(player, "ffa_kits", selectedKit);
-
-            // Announce the kit received to the player
-            player.sendMessage(new TextComponent("You received the " + selectedKit + " kit!"), player.getUUID());
         } else {
             InventoryUtil.loadInventory(player, "ffa_kits", this.id);
-
-            // Announce the kit received to the player
-            player.sendMessage(new TextComponent("You received the " + this.id + " kit!"), player.getUUID());
         }
     }
 }
