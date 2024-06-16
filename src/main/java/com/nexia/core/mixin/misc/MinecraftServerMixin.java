2ff968535a68543afd9cd5d35ebbaf2ee7374c32
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-06-16 21:35:00 +0200

    funny

--- src/main/java/com/nexia/core/mixin/misc/MinecraftServerMixin.java
+++ src/main/java/com/nexia/core/mixin/misc/MinecraftServerMixin.java
@@ -5,10 +5,13 @@ import com.nexia.core.utilities.time.ServerTime;
 import com.nexia.core.utilities.time.ServerType;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.status.ServerStatus;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.players.PlayerList;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,6 +21,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
+
+    @Shadow public abstract int getMaxPlayers();
+
+    @Shadow public abstract int getPlayerCount();
+
+    @Shadow public abstract PlayerList getPlayerList();
 
     @Unique
     boolean firstTickPassed = false;
@@ -48,14 +57,26 @@ public abstract class MinecraftServerMixin {
 
     @ModifyArg(method = "tickServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/status/ServerStatus$Players;setSample([Lcom/mojang/authlib/GameProfile;)V"))
     private GameProfile[] hidePlayers(GameProfile[] gameProfiles) {
-        return (ServerType.returnServer().equals(ServerType.DEV)) ?
-                new GameProfile[]{new GameProfile(Util.NIL_UUID, "§e⟡ you tried ⟡"),
-                        new GameProfile(Util.NIL_UUID, "§eヽ(・∀・)ﾉ"),
-                        new GameProfile(Util.NIL_UUID, " "),
-                        new GameProfile(Util.NIL_UUID, "IPs:"),
-                        new GameProfile(Util.NIL_UUID, "eu.nexia.dev"),
-                        new GameProfile(Util.NIL_UUID, "na.nexia.dev")
+
+        if(ServerType.returnServer().equals(ServerType.DEV)) {
+            return new GameProfile[]{new GameProfile(Util.NIL_UUID, "§e⟡ you tried ⟡"),
+                    new GameProfile(Util.NIL_UUID, "§eヽ(・∀・)ﾉ"),
+                    new GameProfile(Util.NIL_UUID, " "),
+                    new GameProfile(Util.NIL_UUID, "IPs:"),
+                    new GameProfile(Util.NIL_UUID, "eu.nexia.dev"),
+                    new GameProfile(Util.NIL_UUID, "na.nexia.dev")
+                };
         }
-        : gameProfiles;
+
+        return gameProfiles;
+    }
+
+    @ModifyArg(method = "tickServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/status/ServerStatus;setPlayers(Lnet/minecraft/network/protocol/status/ServerStatus$Players;)V"))
+    private ServerStatus.Players hidePlayerCount(ServerStatus.Players players) {
+        if(ServerType.returnServer().equals(ServerType.DEV)) {
+            // can't set current player count for some reason (69)
+            return new ServerStatus.Players(420, 69);
+        }
+        return players;
     }
 }
