2e0875f9d3379b36a0210a60ea893d343f25c9df
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-06-21 17:13:46 +0200

    improve detection (only works if you have fapi)

--- src/main/java/com/nexia/core/networking/NetworkingHandler.java
+++ src/main/java/com/nexia/core/networking/NetworkingHandler.java
@@ -17,7 +17,20 @@ public class NetworkingHandler {
             PlayerDataManager.addPlayerData(handler.player);
 
             if(!ServerPlayNetworking.canSend(handler.player, detectCombatify)) {
-                if(ServerPlayNetworking.canSend(handler.player, new ResourceLocation("fabric", "registry/sync/direct"))) {
+
+                // server.sendMessage(new TextComponent(handler.player.getScoreboardName() + ": " + ServerPlayNetworking.getSendable(handler).toString()), Util.NIL_UUID);
+
+                // 1.20.1 (vfp (+ fapi))
+                // [fabric:container/open, fabric:registry/sync/direct, fabric-screen-handler-api-v1:open_screen]
+
+                // fabric cts (fapi)
+                //[fabric:registry/sync, fabric-screen-handler-api-v1:open_screen]
+
+                // normal cts
+                // []
+
+
+                if(ServerPlayNetworking.canSend(handler.player, new ResourceLocation("fabric", "registry/sync/direct")) || ServerPlayNetworking.canSend(handler.player, new ResourceLocation("fabric", "container/open"))) {
                     PlayerDataManager.get(handler.player).clientType = PlayerData.ClientType.VIAFABRICPLUS;
                     handler.player.connection.disconnect(new TextComponent("§5§lNexia\n§7You need to install §c§lCombatify§7 in order to join the server.\n\n§chttps://modrinth.com/mod/combatify"));
                     return;
