2fe942cae2d811d1906e281c2bfe5c73eb6ddfcb
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-07-25 22:57:40 +0200

    bring over mr stunfs changes

--- src/main/java/com/nexia/core/commands/player/SpectateCommand.java
+++ src/main/java/com/nexia/core/commands/player/SpectateCommand.java
@@ -134,6 +134,7 @@ public class SpectateCommand {
             nexiaExecutor.sendMessage(ChatFormat.nexiaMessage.append(
                     Component.text("That player is not in FFA!").color(ChatFormat.normalColor).decoration(ChatFormat.bold, false)
             ));
+            return 0;
         }
 
         // Check if player is in combat (or full health), then put them in spectator.
