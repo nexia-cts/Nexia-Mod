a6e5133366247df21544e10c08139f3f5c2ffa02
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-08-14 21:26:21 +0200

    move more bug fixes to nexus

--- src/main/java/com/nexia/core/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/nexia/core/mixin/entity/LivingEntityMixin.java
@@ -7,52 +7,15 @@ import net.minecraft.world.CombatRules;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-
-
     public LivingEntityMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
-    }
-
-    @Shadow public abstract double getAttributeValue(Attribute attribute);
-
-    @Shadow public abstract ItemStack getBlockingItem();
-
-    /**
-     * @author NotCoded
-     * @reason Fix Shield Knockback
-     */
-    @Overwrite
-    public void knockback(float f, double d, double e) {
-        LivingEntity instance = (LivingEntity) (Object) this;
-        double g = this.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE);
-        ItemStack itemStack = this.getBlockingItem();
-        if (!itemStack.isEmpty()) {
-            if (instance instanceof Player) g = Math.min(1.0, 1-(1-g)*(1-(double) ShieldItem.getShieldKnockbackResistanceValue(itemStack)));
-            else g = Math.min(1.0, g + (double) ShieldItem.getShieldKnockbackResistanceValue(itemStack));
-        }
-
-        f = (float)((double)f * (1.0 - g));
-        if (!(f <= 0.0F)) {
-            instance.hasImpulse = true;
-            Vec3 vec3 = instance.getDeltaMovement();
-            Vec3 vec32 = (new Vec3(d, 0.0, e)).normalize().scale(f);
-            instance.setDeltaMovement(vec3.x / 2.0 - vec32.x, instance.isOnGround() ? Math.min(0.4, (double)f * 0.75) : Math.min(0.4, vec3.y + (double)f * 0.5),vec3.z / 2.0 - vec32.z);
-        }
     }
 
     @Redirect(method = "getDamageAfterMagicAbsorb", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/CombatRules;getDamageAfterMagicAbsorb(FF)F"))
