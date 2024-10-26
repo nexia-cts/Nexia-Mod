dcbdda3835ab8902653ecaaca416341db274a1db
database-test <143010276+database-test@users.noreply.github.com>
2024-10-26 12:12:23 -0400

    hope to fix null world issue

--- src/main/java/com/nexia/minigames/games/bridge/BridgeMap.java
+++ src/main/java/com/nexia/minigames/games/bridge/BridgeMap.java
@@ -9,32 +9,21 @@ import java.util.List;
 public class BridgeMap {
 
     public static List<BridgeMap> bridgeMaps = new ArrayList<>();
-
+    public static BridgeMap STADIUM = new BridgeMap("stadium", "Stadium", 5, new BlockPos(68, 106, 86), new BlockPos(-68, 73, -86), new EntityPos(0, 80, -10), new EntityPos(0, 80, 10, -180, 0), new BlockPos(-3, 82, -52), new BlockPos(3, 79, -50), new BlockPos(3, 82, 52), new BlockPos(-3, 79, 50));
     public String id;
-
     public String name;
-
     public int maxGoals;
-
     public BlockPos corner1;
-
     public BlockPos corner2;
-
-
+    public BlockPos team1goalCorner1;
+    public BlockPos team1goalCorner2;
+    public BlockPos team2goalCorner1;
+    public BlockPos team2goalCorner2;
     public EntityPos team1Pos;
     public EntityPos team2Pos;
 
-    public static BridgeMap STADIUM = new BridgeMap("stadium", "Stadium", 5, new BlockPos(68, 106, 86), new BlockPos(-68, 73, -86), new EntityPos(0, 80, -10), new EntityPos(0, 80, 10, -180, 0));
 
-
-    public static BridgeMap identifyMap(String name) {
-        for(BridgeMap map : BridgeMap.bridgeMaps) {
-            if(map.id.equalsIgnoreCase(name)) return map;
-        }
-        return null;
-    }
-
-    public BridgeMap(String id, String name, int maxGoals, BlockPos corner1, BlockPos corner2, EntityPos team1Pos, EntityPos team2Pos) {
+    public BridgeMap(String id, String name, int maxGoals, BlockPos corner1, BlockPos corner2, EntityPos team1Pos, EntityPos team2Pos, BlockPos team1goalCorner1, BlockPos team1goalCorner2, BlockPos team2goalCorner1, BlockPos team2goalCorner2) {
         this.id = id;
         this.name = name;
         this.maxGoals = maxGoals;
@@ -45,8 +34,19 @@ public class BridgeMap {
         this.corner1 = corner1;
         this.corner2 = corner2;
 
+        this.team1goalCorner1 = team1goalCorner1;
+        this.team1goalCorner2 = team1goalCorner2;
 
+        this.team2goalCorner1 = team2goalCorner1;
+        this.team2goalCorner2 = team2goalCorner2;
 
         BridgeMap.bridgeMaps.add(this);
+    }
+
+    public static BridgeMap identifyMap(String name) {
+        for (BridgeMap map : BridgeMap.bridgeMaps) {
+            if (map.id.equalsIgnoreCase(name)) return map;
+        }
+        return null;
     }
 }
