a0b3b70aafcc9caf9dd56cec59ab321221e36834
database-test <143010276+database-test@users.noreply.github.com>
2024-10-27 10:12:00 -0400

    idk anymore

--- src/main/java/com/nexia/core/mixin/block/EndPortalBlockMixin.java
+++ src/main/java/com/nexia/core/mixin/block/EndPortalBlockMixin.java
@@ -1,14 +1,13 @@
 package com.nexia.core.mixin.block;
-import com.nexia.minigames.games.bridge.BridgeGame;
+
 import com.nexia.base.player.NexiaPlayer;
 import com.nexia.base.player.PlayerDataManager;
-import com.nexia.core.NexiaCore;
-import com.nexia.minigames.games.duels.util.player.DuelsPlayerData;
+import com.nexia.minigames.games.bridge.BridgeGame;
+import com.nexia.minigames.games.bridge.util.player.BridgePlayerData;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EndPortalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,8 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.nexia.minigames.games.bridge.BridgeGame.team1;
-import static com.nexia.minigames.games.bridge.BridgeGame.team2;
+import static com.nexia.core.NexiaCore.BRIDGE_DATA_MANAGER;
 
 @Mixin(EndPortalBlock.class)
 public class EndPortalBlockMixin {
@@ -26,16 +24,21 @@ public class EndPortalBlockMixin {
     )
     private void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity, CallbackInfo ci) {
         if (!(entity instanceof ServerPlayer serverPlayer)) return;
+
         NexiaPlayer nexiaPlayer = new NexiaPlayer(serverPlayer);
-        if (nexiaPlayer.unwrap() == entity && nexiaPlayer.hasTag("in_bridge_game")) {
 
-                BridgeGame.goal(nexiaPlayer);
+        if (nexiaPlayer.hasTag("in_bridge_game")) {
 
+            BridgePlayerData data = (BridgePlayerData) PlayerDataManager.getDataManager(BRIDGE_DATA_MANAGER).get(nexiaPlayer);
 
+            if (data.team == BridgeGame.team1) {
 
+                BridgeGame.goal(nexiaPlayer, "team1");
+            }
+            if (data.team == BridgeGame.team2) {
 
-
+                BridgeGame.goal(nexiaPlayer, "team2");
+            }
         }
-
-}
+    }
 }
