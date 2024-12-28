7b8089d4e855ad11f6bca33478d28664e6f3c31e
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-12-28 16:55:27 +0100

    crazy fix

--- src/main/java/com/nexia/core/listeners/nexus/LivingEntityHealListener.java
+++ src/main/java/com/nexia/core/listeners/nexus/LivingEntityHealListener.java
@@ -3,11 +3,13 @@ package com.nexia.core.listeners.nexus;
 import com.nexia.base.player.NexiaPlayer;
 import com.nexia.ffa.uhc.utilities.FfaUhcUtil;
 import com.nexia.nexus.api.event.entity.LivingEntityHealEvent;
+import com.nexia.nexus.api.world.entity.player.Player;
 
 public class LivingEntityHealListener {
     public void registerListener() {
         LivingEntityHealEvent.BACKEND.register(livingEntityHealEvent -> {
-            if (!(livingEntityHealEvent.getEntity() instanceof NexiaPlayer nexiaPlayer)) return;
+            if (!(livingEntityHealEvent.getEntity() instanceof Player player)) return;
+            NexiaPlayer nexiaPlayer = new NexiaPlayer(player);
 
             if (FfaUhcUtil.INSTANCE.isFfaPlayer(nexiaPlayer) && livingEntityHealEvent.getCause() == LivingEntityHealEvent.HealCause.NATURAL_REGENERATION) {
                 livingEntityHealEvent.setCancelled(true);
